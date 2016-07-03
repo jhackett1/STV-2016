434b75ba880a16ad93e58d29f2e833e69d9c52cb
jhackett1 <hello@joshuahackett.com>
2016-07-03 14:32:25 +0100

    Redesign

--- front-page.php
+++ front-page.php
@@ -1,7 +1,6 @@
 <?php get_header(); ?>
 
 <!-- A responsive hero section -->
-
 <section id="hero">
 
   <!-- <div class="bg" style="background-image: url(<?php bloginfo('template_directory'); ?>/img/bg.jpg"></div> -->
@@ -11,7 +10,7 @@
     Your browser does not support the video tag.
   </video>
 
-  <div class="grad"></div>
+  <!-- <div class="grad"></div> -->
 
   <div class="container">
 
@@ -32,10 +31,13 @@
 <!-- An embedded Youtube video in the "Watch" section -->
 
 <section id="watch">
+
+  <div id="slice"></div>
+
   <div class="container">
 
     <h3>Watch</h3>
-    <p>Marfa lo-fi wayfarers, chia hammock whatever skateboard gentrify everyday carry deep v street art put a bird on it occupy mustache forage.</p>
+    <p>We are the University of Westminster’s student-run TV station. We cover events, produce highlights, news roundups and live broadcasts, bringing together thousands of students across four campuses.</p>
 
     <div class="video-container">
       <iframe width="560" height="315" src="https://www.youtube.com/embed/1lAobSkstCc" frameborder="0" allowfullscreen></iframe>
@@ -53,28 +55,87 @@
 <!-- The team gallery -->
 
 <section id="team">
+
+  <div id="grad"></div>
+
   <div class="container">
 
-    <h3>Meet the team</h3>
-    <p>Description goes here</p>
+    <h3>What's going on?</h3>
+    <p>Take a look at our current projects...</p>
+
+      <script>
+          $(document).ready(function(){
+
+              $('.social-feed-container').socialfeed({
+
+                  facebook: {
+                      accounts: ['@smoketv'],
+                      limit: 5,
+                      access_token: '150849908413827|a20e87978f1ac491a0c4a721c961b68c'
+                  },
+
+                  instagram: {
+                      accounts: ['smoketvcrew'],
+                      limit: 2,
+                      client_id: '8350ffadd7eb487893a3698307ea67cb',
+                      access_token: ''
+                  },
+
+                  twitter:{
+                      accounts: ['@wminsmoketv'],                      //Array: Specify a list of accounts from which to pull tweets
+                      limit: 5,                                   //Integer: max number of tweets to load
+                      consumer_key: 'DeiNAbk35FM7HtuHI7rwUvgCh',          //String: consumer key. make sure to have your app read-only
+                      consumer_secret: 'QlD2zJn0IzAfoIqIzZ1kNheeJir4atuLnWCZHPtVvT0pzljdjW',//String: consumer secret key. make sure to have your app read-only
+                   },
+
+                  // GENERAL SETTINGS
+                  length:400,                                      //Integer: For posts with text longer than this length, show an ellipsis.
+                  show_media:true,
+                  date_format: "s",
+                  template: "<?php echo get_template_directory_uri(); ?>/bower_components/social-feed/template.html",
+                  callback: function() {                          //Function: This is a callback function which is evoked when all the posts are collected and displayed
+                      console.log("All posts collected!");
+                  }
+
+              });
+          });
+          console.log("testing");
+      </script>
+
+      <div class="social-feed-container" style="max-width: 600px; margin: 20px auto"></div>
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   </div>
 </section>
 
 <!-- A mailing list subscribe CTA -->
 
-<section id="subscribe">
+<!-- <section id="subscribe">
   <div class="container">
 
     <h3>Get on the list</h3>
     <p>Description goes here</p>
 
   </div>
-</section>
+</section> -->
 
 <!-- Service triplet boxes linking to email. -->
 
 <section id="services">
+  <div id="grad"></div>
+
   <div class="container">
 
     <h3>We cover events</h3>
--- header.php
+++ header.php
@@ -23,3 +23,23 @@
 </header>
 
 <?php get_template_part( "search-modal" ); ?>
+
+
+
+
+<script src="<?php echo get_template_directory_uri(); ?>/bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Social-feed css -->
+<link href="<?php echo get_template_directory_uri(); ?>/bower_components/social-feed/css/jquery.socialfeed.css" rel="stylesheet" type="text/css">
+
+<!-- jQuery -->
+<script src="<?php echo get_template_directory_uri(); ?>/bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Codebird.js - required for TWITTER -->
+<script src="<?php echo get_template_directory_uri(); ?>/bower_components/codebird-js/codebird.js"></script>
+<!-- doT.js for rendering templates -->
+<script src="<?php echo get_template_directory_uri(); ?>/bower_components/doT/doT.min.js"></script>
+<!-- Moment.js for showing "time ago" and/or "date"-->
+<script src="<?php echo get_template_directory_uri(); ?>/bower_components/moment/min/moment.min.js"></script>
+<!-- Moment Locale to format the date to your language (eg. italian lang)-->
+<script src="<?php echo get_template_directory_uri(); ?>/bower_components/moment/locale/it.js"></script>
+<!-- Social-feed js -->
+<script src="<?php echo get_template_directory_uri(); ?>/bower_components/social-feed/js/jquery.socialfeed.js"></script>
